cdca23008f3e62800a3223c0b3af2e7513d34167
ssaatya <167321180+ssaatya@users.noreply.github.com>
2024-05-10 18:52:34 +0700

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,10 @@
+import random
+# function to generate a random password
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
